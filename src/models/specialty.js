34a09fcbdf853cbbe97a0051dc64d394a650168d
nguyễn hoàn <hoannk310@gmail.com>
2021-10-10 23:59:06 +0700

    update model

--- src/models/specialty.js
+++ src/models/specialty.js
@@ -3,7 +3,7 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Allcode extends Model {
+    class User extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
@@ -13,14 +13,16 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     };
-    Allcode.init({
-        key: DataTypes.STRING,
-        type: DataTypes.STRING,
-        valueEn: DataTypes.STRING,
-        valueVi: DataTypes.STRING,
+    User.init({
+        email: DataTypes.STRING,
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        address: DataTypes.STRING,
+        gender: DataTypes.BOOLEAN,
+        roleid: DataTypes.STRING,
     }, {
         sequelize,
-        modelName: 'Allcode',
+        modelName: 'User',
     });
-    return Allcode;
+    return User;
 };
